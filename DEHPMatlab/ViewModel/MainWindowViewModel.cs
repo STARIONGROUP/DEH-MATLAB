fa665dc5f7e9e1fb14989ba04f1c88912b487440
antoineatrhea <a.theate@rheagroup.com>
2022-01-20 16:25:27 +0100

    Initial project; fixes #2

---
+++ DEHPMatlab/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindowViewModel.cs" company="RHEA System S.A.">
+// Copyright (c) 2020-2022 RHEA System S.A.
+// 
+// Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Antoine Théate.
+// 
+// This file is part of DEHPMatlab
+// 
+// The DEHPMatlab is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+// 
+// The DEHPMatlab is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DEHPMatlab.ViewModel
+{
+    using DEHPCommon.Services.NavigationService;
+    using DEHPCommon.UserInterfaces.Behaviors;
+
+    using DEHPMatlab.ViewModel.Interfaces;
+
+    /// <summary>
+    /// Represents the view model for <see cref="Views.MainWindow"/>
+    /// </summary>
+    public class MainWindowViewModel : IMainWindowViewModel
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="ISwitchLayoutPanelOrderBehavior"/>
+        /// </summary>
+        public ISwitchLayoutPanelOrderBehavior SwitchPanelBehavior { get; set; }
+
+        /// <summary>
+        /// The <see cref="INavigationService"/>
+        /// </summary>
+        private readonly INavigationService navigationService;
+
+        /// <summary>
+        /// Create a new instance of <see cref="MainWindowViewModel"/>
+        /// </summary>
+        /// <param name="navigationService">A <see cref="INavigationService"/></param>
+        public MainWindowViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+    }
+}
